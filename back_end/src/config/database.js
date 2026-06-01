ac777c2ac34321c848f040fdc63b8cc418c71062
YOBS KOLER <yobskoler9@gmail.com>
2026-06-01 21:17:21 +0100

    switch to postgre..sql

--- back_end/src/config/database.js
+++ back_end/src/config/database.js
@@ -1,22 +1,33 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres", 
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, 
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
-  },
-);
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+          ssl:
+            process.env.NODE_ENV === "production"
+              ? { require: true, rejectUnauthorized: false }
+              : false,
+        },
+      },
+    );
 
 module.exports = sequelize;
